03fcd5d6cd7f87ac115fcd2749d7062986d1d195
Walter Torres <walter.torres@duke.edu>
2021-01-13 16:32:07 +0800

    update navpane

--- theory/utils.py
+++ theory/utils.py
@@ -1,9 +1,20 @@
 import panel as pn
+import numpy as np
+
 navbar = """
 [Home](/home) | 
+[2D Numerical Solution](/diagnostics_2D) | 
 [ODE Numerical Solution](/ode_solution-app) | 
 [ODE Stability](/ode_stability-app)
 """
 
 navpane = pn.pane.Markdown(navbar)
+
+def border(ds):
+    x = ds.x_psi.values
+    y = ds.y_psi.values
+    xo = np.concatenate([x[0,:-1], x[:-1,-1], x[-1,::-1], x[-2:0:-1,0]])
+    yo = np.concatenate([y[0,:-1], y[:-1,-1], y[-1,::-1], y[-2:0:-1,0]])
+    coords = np.vstack((xo,yo)).T
+    return coords
 
