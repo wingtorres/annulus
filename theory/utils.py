eaff7d42bd8763d94ec963248e2489812f53101f
Walter Torres <walter.torres@duke.edu>
2021-02-16 13:59:09 +0800

    add plot to make border commented out
    Former-commit-id: 0297e37e901dd979c6e29a908e512a159382e3e9

--- theory/utils.py
+++ theory/utils.py
@@ -19,5 +19,6 @@ def border(ds):
     xo = np.concatenate([x[0,:-1], x[:-1,-1], x[-1,::-1], x[-2:0:-1,0]])
     yo = np.concatenate([y[0,:-1], y[:-1,-1], y[-1,::-1], y[-2:0:-1,0]])
     coords = np.vstack((xo,yo)).T
+    #hv.Curve(coords).opts(color = "k", line_width = 1)
     return coords
 
