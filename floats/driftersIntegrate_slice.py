606a939e59e9030034a43541e1066ec7f33bc350
walter.torres@duke.edu <walter.torres@duke.edu>
2021-08-31 11:12:20 +0800

    update imports

--- floats/driftersIntegrate_slice.py
+++ floats/driftersIntegrate_slice.py
@@ -1,50 +1,18 @@
 import os
 import sys
-import xarray as xr
-# import xgcm
-import numpy as np
 import math
-import cartopy.crs as ccrs
-import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 
 from datetime import timedelta as delta
 import cftime
 
 from parcels import AdvectionRK4, ErrorCode, FieldSet, JITParticle, ParticleFile, ParticleSet, Variable, plotting, Kernel
 
-home = os.environ['HOME']
-
 import romspy
 import romspy.accessor
-from romspy import circulation as pcc
-from romspy.grid import cart2polar
 
-
-## 1/12 Annulus
-gridname = home + '/Dropbox/Oceanography/Projects/annulus/grid/ann_grid_smol.nc'
-avgname = '../output/annulus_sector_3D/smol_lat_-30_z0_0.25/results/ocean_avg.nc'
-os.chdir(home + '/Dropbox/Oceanography/Projects/annulus/floats/')
-
-def roms2tracks(gridname, avgname, xp, yp):
-
-ds = xr.open_dataset(filename, decode_times= False)#.isel(ocean_time = -1)
-dg = xr.open_dataset(gridname)
-
-time = ds['ocean_time']
-x = dg['x_psi'].values
-y = dg['y_psi'].values
-r,t = np.arctan2(y,x), np.hypot(x,y)
-
-#Transform grid to polar
-lon, lat = cart2polar(x,y)
-
-ds.xwit.make_grid
-ds.xwit.lagrangian_velocity(rotate = True)
-
-#data = {'U': -ds.xwit.ds.ubar_lagrangian_psi.values, 'V': -ds.xwit.ds.vbar_lagrangian_psi.values} #2D velocity
-data = {'U': ds.xwit.ds.vbar_lagrangian_psi.values, 'V': -ds.xwit.ds.ubar_lagrangian_psi.values} #2D velocity cartesian
-dimensions = {'lon': x, 'lat': y}
-fieldset = FieldSet.from_data(data, dimensions, transpose = False, mesh = "flat")
+home = os.environ['HOME']
 
 class witParticle(JITParticle):
     theta = Variable('theta', dtype=np.float32, initial=0.)
@@ -71,32 +39,54 @@ def DeleteParticle(particle, fieldset, time):
     print("Deleting particle")
     particle.delete()
 
-recovery = {ErrorCode.ErrorOutOfBounds: DeleteParticle,
-            ErrorCode.ErrorThroughSurface: DeleteParticle}
+def roms2tracks(gridname, filename, outfile = "annfloats_slice.nc"):
+    
+    print(gridname, filename, outfile)
+    ds = xr.open_dataset(filename, decode_times = False)
+    dg = xr.open_dataset(gridname)
+
+    time = ds['ocean_time']
+    x = dg['x_psi'].values
+    y = dg['y_psi'].values
+    r,t = np.arctan2(y,x), np.hypot(x,y)
+
+    ds.xwit.make_grid
+    ds.xwit.lagrangian_velocity(rotate = True)
+
+    #data = {'U': -ds.xwit.ds.ubar_lagrangian_psi.values, 'V': -ds.xwit.ds.vbar_lagrangian_psi.values} #2D velocity
+    data = {'U': ds.xwit.ds.vbar_lagrangian_psi.values, 'V': -ds.xwit.ds.ubar_lagrangian_psi.values} #2D velocity cartesian
+    dimensions = {'lon': x, 'lat': y}
+    fieldset = FieldSet.from_data(data, dimensions, transpose = False, mesh = "flat")
+
+    recovery = {ErrorCode.ErrorOutOfBounds: DeleteParticle,
+                ErrorCode.ErrorThroughSurface: DeleteParticle}
+
+    nt = 2**6
+    nr = 2**5
+    rp, tp = np.meshgrid( np.linspace(10.5e3, 13.5e3, nr), np.linspace(-14*np.pi/180 , 14*np.pi/180, nt) ) 
+    xp, yp = rp*np.cos(tp), rp*np.sin(tp)
+    xp, yp = xp.flatten().tolist(), yp.flatten().tolist()
+
+    pset = ParticleSet.from_list(fieldset = fieldset, pclass = witParticle, lon = xp, lat = yp )
+
+    # fieldset.U.show()
+    # plt.pcolormesh(x, y, dg.h.values[:-1,:-1])
+    # plt.scatter(xp,yp, s = 10, color = "k")
+    # plt.show()
+    #assert False
+
+    kernels = AdvectionRK4 + pset.Kernel(PeriodicParticle)
+    output_file = pset.ParticleFile(name = outfile, outputdt = delta(seconds = 30) )
+    pset.execute( kernels, runtime = delta(hours = 60.0), dt = delta(seconds = 30), output_file = output_file, recovery = recovery)
+    output_file.export()
+    output_file.close()
 
 
+def main():
+    gridname = sys.argv[1]
+    avgname = sys.argv[2]
+    outname = sys.argv[3]
+    roms2tracks(gridname, avgname, outname)
 
-#lonp, latp = np.meshgrid( np.linspace(-14, 14, nt), np.linspace( 89.885, 89.909, nr ) )
-
-
-lonp = lonp.flatten().tolist()
-latp = latp.flatten().tolist()
-
-xp, yp = xp.flatten().tolist(), yp.flatten().tolist()
-# xp, yp = 13000, 0
-
-pset = ParticleSet.from_list(fieldset = fieldset, pclass = witParticle, lon = xp, lat = yp )
-
-# fieldset.U.show()
-# plt.pcolormesh(x, y, dg.h.values[:-1,:-1])
-# plt.scatter(xp,yp, s = 10, color = "k")
-# plt.show()
-#assert False
-
-kernels = AdvectionRK4 + pset.Kernel(PeriodicParticle)
-output_file = pset.ParticleFile(name= "annfloats_slice", outputdt = delta(seconds = 30) )
-pset.execute( kernels, runtime = delta(hours = 60.0), dt = delta(seconds = 30), output_file = output_file, recovery = recovery )
-output_file.export()
-output_file.close()
-
-#sandbox
+if __name__ == "__main__":
+    main()
